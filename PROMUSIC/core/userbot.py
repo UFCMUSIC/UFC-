04bf00c2b7af0948e29f23efcbbfaad5fcd73d07
BABY-X-MUSIC <DEEPAKKUMARCSC911@GMAIL.COM>
2025-03-10 18:27:42 +0530

    Update userbot.py

--- PROMUSIC/core/userbot.py
+++ PROMUSIC/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("untold_coder")
-                await self.one.join_chat("untold_coder")
+                await self.one.join_chat("UFC_NETWORK")
+                await self.one.join_chat("UFC_NETWORK")
             except:
                 pass
             assistants.append(1)
