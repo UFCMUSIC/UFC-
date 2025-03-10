88104a6ec8d98571a012dc7c71726216100cc91a
BABY-X-MUSIC <DEEPAKKUMARCSC911@GMAIL.COM>
2025-03-10 17:57:47 +0530

    Update untold.py

--- PROMUSIC/plugins/tools/untold.py
+++ PROMUSIC/plugins/tools/untold.py
@@ -32,13 +32,13 @@ from PROMUSIC.utils.database import get_assistant, is_active_chat
 @app.on_message(filters.command("clone"))
 async def clones(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://graph.org/file/3d3efd2d7b620df244d4a-42e0d3f34444bf754b.jpg",
+        photo=f"https://files.catbox.moe/hbkwsg.jpg",
         caption=f"""**🙂You Are Not Sudo User So You Are Not Allowed To Clone Me.**\n**😌Click Given Below Button And Host Manually Otherwise Contact Owner Or Sudo Users For Clone.**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "❮Oᴡɴᴇʀ❯", url=f"https://t.me/Rocky_dtm"
+                        "❮Oᴡɴᴇʀ❯", url=f"https://t.me/UFC_INOCENT"
                     )
                 ]
             ]
