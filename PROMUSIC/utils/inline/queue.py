cf2b20ac5c7975db517cae1233aa3f99d5e385db
BABY-X-MUSIC <DEEPAKKUMARCSC911@GMAIL.COM>
2025-03-10 17:40:15 +0530

    Update queue.py

--- PROMUSIC/utils/inline/queue.py
+++ PROMUSIC/utils/inline/queue.py
@@ -105,7 +105,7 @@ def queuemarkup(_, vidid, chat_id):
         [
             InlineKeyboardButton(
                 text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/Rocky_dtm",
+                url="https://t.me/UFC_NETWORK",
             ),
         ],
     ]
