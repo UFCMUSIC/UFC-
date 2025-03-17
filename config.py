d6c105dfbf1810ee1b8cc05d493f5fee5d418a71
UFCMUSIC <babymusic936@gmail.com>
2025-03-17 20:36:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BABY-X-MUSIC/PRO",
+    "https://github.com/UFCMUSIC/UFC-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
